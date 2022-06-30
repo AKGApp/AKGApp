fd58af4193d0fa78fb863d7eaaa90ac81e9bd078
salmeen9 <salmeen@akgapp.com>
2022-06-30 21:52:38 +0300

    Added all scenes and ClearUI

--- Assets/Scripts/UIScripts/MVP_UIManager.cs
+++ Assets/Scripts/UIScripts/MVP_UIManager.cs
@@ -9,16 +9,16 @@ public class MVP_UIManager : MonoBehaviour
 
     //TODO: Add the public Pages and Scenes List
     //* All first letters are capital in game reference
-    [Header("Welcome & Signup")]
+    [Header("Welcome & SignIn")]
     [SerializeField]
     private GameObject gameTitleScreen;
     [SerializeField]
     private GameObject signInScreen;
-    [SerializeField]
-    private GameObject signUpScreens;
 
     [Space(10f)]
     [Header("Signup")]
+    [SerializeField]
+    private GameObject signUpScreens;
     [SerializeField]
     private GameObject welcomeMessageScreen;
     [SerializeField]
@@ -44,6 +44,7 @@ public class MVP_UIManager : MonoBehaviour
     private GameObject childAddedSuccessfullyMessageScreen; // spelling error in word successfully
     [SerializeField]
 
+    [Space(10f)]
     private GameObject userEntryScreen; // spelling error in the word Entry
     [SerializeField]
     private GameObject gameMenuScreen;
@@ -75,6 +76,21 @@ public class MVP_UIManager : MonoBehaviour
     {
         //TODO: Set All UI elements visibility to false
         gameTitleScreen.SetActive(false);
+        signInScreen.SetActive(false);
+        signUpScreens.SetActive(false);
+        welcomeMessageScreen.SetActive(false);
+        addUserInfoScreen.SetActive(false);
+        chooseGuardianRoleScreen.SetActive(false);
+        userAdditionalInfoScreen.SetActive(false);
+        setYourGuardianPasswordScreen.SetActive(false);
+        letsAddYourKidsScreen.SetActive(false);
+        addChildrenScreens.SetActive(false);
+        addChildrenScreen.SetActive(false);
+        yourChildInformationScreen.SetActive(false);
+        childAddedSuccessfullyMessageScreen.SetActive(false);
+        userEntryScreen.SetActive(false);
+        gameMenuScreen.SetActive(false);
+
 
     }
 
@@ -85,5 +101,88 @@ public class MVP_UIManager : MonoBehaviour
         gameTitleScreen.SetActive(true);
     }
 
+    public void ShowSignInScreen()
+    {
+        ClearUI();
+        signInScreen.SetActive(true);
+    }
+
+    public void ShowSignUpScreens()
+    {
+        ClearUI();
+        signUpScreens.SetActive(true);
+    }
+
+    public void ShowWelcomeMessageScreen()
+    {
+        ClearUI();
+        welcomeMessageScreen.SetActive(true);
+    }
+
+    public void ShowAddUserInfoScreen()
+    {
+        ClearUI();
+        addUserInfoScreen.SetActive(true);
+    }
+
+    public void ShowChooseGuardianRoleScreen()
+    {
+        ClearUI();
+        chooseGuardianRoleScreen.SetActive(true);
+    }
+
+    public void ShowUserAdditionalInfoScreen()
+    {
+        ClearUI();
+        userAdditionalInfoScreen.SetActive(true);
+    }
+
+    public void ShowSetYourGuardianPasswordScreen()
+    {
+        ClearUI();
+        setYourGuardianPasswordScreen.SetActive(true);
+    }
+
+    public void ShowLetsAddYourKidsScreen()
+    {
+        ClearUI();
+        letsAddYourKidsScreen.SetActive(true);
+    }
+
+    public void ShowAddChildrenScreens()
+    {
+        ClearUI();
+        addChildrenScreens.SetActive(true);
+    }
+
+    public void ShowAddChildrenScreen()
+    {
+        ClearUI();
+        addChildrenScreen.SetActive(true);
+    }
+
+    public void ShowYourChildInformationScreen()
+    {
+        ClearUI();
+        yourChildInformationScreen.SetActive(true);
+    }
+
+    public void ShowChildAddedSuccessfullyMessageScreen()
+    {
+        ClearUI();
+        childAddedSuccessfullyMessageScreen.SetActive(true);
+    }
+
+    public void ShowUserEntryScreen()
+    {
+        ClearUI();
+        userEntryScreen.SetActive(true);
+    }
+
+    public void ShowGameMenuScreen()
+    {
+        ClearUI();
+        gameMenuScreen.SetActive(true);
+    }
 
 }
